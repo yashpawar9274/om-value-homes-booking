c16f9945a0fab13675b2d1a2c86ea0b4cc0f0165
Yash Pawar <yash.pawar@theemcoe.org>
2026-08-01 11:39:23 +0530

    Update SiteHeader.tsx

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -26,15 +26,7 @@ export default function SiteHeader() {
           priority
         />
       </Link>
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-13RK5765KB"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-13RK5765KB');
-</script>
       <button
         className="menu-toggle"
         type="button"
